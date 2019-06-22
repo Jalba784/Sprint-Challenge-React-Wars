9ffa122949619bff7c873ac20d78354b9df9ff3f
josepaniagua <jpanjr@gmail.com>
2019-06-21 17:53:27 -0700

    listofcharacters function skeleton

---
+++ starwars/src/components/ListOfCharacters.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CharacterDef from './CharacterDef.js';
+import '../App.css';
+
+const ListOfChracters = props => {
+    return (
+        //
+    );
+};
+
+export default ListOfChracters
