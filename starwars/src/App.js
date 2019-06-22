86426d892227d68ba15013b032e6db275ed4c772
josepaniagua <jpanjr@gmail.com>
2019-06-21 18:06:24 -0700

    added listofchararacters component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import './components/StarWars.css';
+import ListOfChracters from "./components/ListOfCharacters";
 
 class App extends Component {
   constructor() {
@@ -33,6 +35,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <ListOfChracters characters={this.state.starwarsChars} />
       </div>
     );
   }
